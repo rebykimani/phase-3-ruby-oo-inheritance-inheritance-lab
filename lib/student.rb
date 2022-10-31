88369aaa8b1ec55a2c48052426aeb03ca61e36ec
rebykimani <beckireby15@gmail.com>
2022-10-31 17:02:14 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative './user'
+class Student < User
+    attr_accessor :knowledge
+    
+    @knowledge=[]
+
+    def initialize
+        @knowledge=[]
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
 
 end
